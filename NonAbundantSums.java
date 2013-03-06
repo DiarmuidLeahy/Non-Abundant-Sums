de7b2561af4dcd117a12da4625a6be587f9ff2de
Zachery Metcalf <zachery.metcalf@gmail.com>
2013-03-05 19:33:07 -0700

    Started Logic

--- NonAbundantSums.java
+++ NonAbundantSums.java
@@ -1,3 +1,58 @@
+import java.util.ArrayList;
+
 public class NonAbundantSums {
     
+    ArrayList<Integer> abundantNumbers = new ArrayList<Integer>();
+    ArrayList<Integer> listOfNonSum = new ArrayList<Integer>();
+    
+    public NonAbundantSums() {
+
+
+    }
+    
+    
+    
+    private int findListOfNonSum() {
+        for(i = 1; i < 28123; i++) {
+            if(checkAbundance(i))
+        }
+    }
+    
+    private boolean checkAbundance(int checkNum) {
+        ArrayList<Integer> checkNumDivisors = new ArrayList<Integer>();
+        int sumOfDivisors = 0;
+        
+        checkNumDivisors = divisorList(checkNum);
+        
+        for(int x : checkNumDivisors) {
+            sumOfDivisors += x;
+        }
+        if(sumOfDivisors > checkNum) {
+            return true;
+        }
+        return false;        
+    }
+    
+    private boolean checkIfProductOfAbundant(int checkNum) {
+        for(int x : abundantNumbers) {
+            for(int y: abundantNumber) {
+                if((x + y) == checkNum) {
+                    return true;
+                }
+            }
+        }
+        return false;    
+    }
+    
+    private ArrayList<Integer> divisorList (int divisor) {
+        
+        ArrayList<Integer> dList = new ArrayList<Integer>();
+        
+        for(int i = 2; i <= divisor; i++) {
+            if(divisor % i == 0) {
+                dList.add(divisor / i);
+            }
+        }    
+        return dList; 
+    } //End divisorList method
 }
--- NonAbundantSumsTest.java
+++ NonAbundantSumsTest.java
@@ -1,6 +1,6 @@
 public class NonAbundantSumsTest {
     
     public static void main(String [] args) {
-        
+        NonAbundantSums scoreTest = new NonAbundantSums();
     }
 }
